ed3a6e9653167b93723605c71df346b343ba897f
Xinyi Lin <kevinmacbook@Xinyis-MacBook-Pro.local>
2022-09-25 11:17:46 -0400

    Makes minor update to about page

--- src/components/home/AboutMe.js
+++ src/components/home/AboutMe.js
@@ -23,16 +23,9 @@ function AboutMe() {
                     </Col>
                     <Col lg={6} xl={7} xxl={8} >
                     <p className="lead" >
-                        I am a Junior studying Computer Science at CUNY Hunter College. 
-                        While pursuing my degree, I met many others sharing the same interest as me. And the more I learned, the more I realized there are left to be learn.
-                    </p>
-                    <p className="lead" >
-                        The industry has been growing ever since the start and 
-                        being able to grow along with it makes the process even more enjoyable. 
-                    </p>
+                        Xinyi is a Senior studying Computer Science and Media Studies at CUNY Hunter College, anticipating to graduate Fall 2023. </p>
                     <p className="lead">
-                        Other than programming, I enjoy drawing, reading wu-xia novels, 
-                        taking pictures and playing video games like League of Legends. 
+                        
                     </p>
                     </Col>
                 </Row>
